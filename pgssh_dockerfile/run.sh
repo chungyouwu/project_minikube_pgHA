607df1b2542709db00e04be9ead28d902a304cb6
chungyouwu <chungyou0118@gmail.com>
2024-06-20 23:10:15 +0800

    first commit

---
+++ pgssh_dockerfile/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 遇到錯誤立即停止
+set -e
+
+# 啟動 sshd 服務
+/usr/sbin/sshd &
+
+# Start PostgreSQL in the background
+docker-entrypoint.sh postgres &
+
+# Wait for PostgreSQL to start
+until pg_isready -h localhost -p 5432; do
+    sleep 1;
+done
+
+# Run a loop to keep the container alive
+while true; do
+    sleep 60;
+done
