350beff01780e1bbc2d47e420dbb907149af267b
chungyouwu <chungyou0118@gmail.com>
2024-07-26 14:14:58 +0800

    pressure tets

---
+++ pressure_test.sql
@@ -0,0 +1,10 @@
+DO $$
+DECLARE
+    reason TEXT := 'test't;
+    method PAYBY := 'credit card'::payby;
+    m_from INT := 1;
+    m_to INT := FLOOR(random() * 1000 + 1)::int;
+    amount NUMERIC := 1;
+BEGIN
+    CALL transfer_funds(m_from, m_to, amount, reason, method);
+END $$;
