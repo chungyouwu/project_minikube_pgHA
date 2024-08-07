a8f2df60c364372ae0e5fa0b1a6de9bf97a24353
chungyouwu <chungyou0118@gmail.com>
2024-08-07 23:06:58 +0800

    email fun

--- tools_python/tool_email.py
+++ tools_python/tool_email.py
@@ -1,47 +1,28 @@
 import smtplib
 from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import configparser
 
+config = configparser.ConfigParser()
+with open('email.ini', 'r', encoding='utf-8') as f:
+    config.read_file(f) 
 
+smtp_user = config['smtp']['user']
+smtp_password = config['smtp']['password']
+smtp_server = config['smtp']['server']
+smtp_port = config['smtp']['port']
 
-def send_email(subject, body, to_email):
-    # 讀取配置文件
-    config = configparser.ConfigParser()  # 避免在程式碼中出現敏感資訊
-    config.read('email.ini')
-    smtp_user = config['smtp']['user']
-    smtp_password = config['smtp']['password']
+msg = MIMEText('Python 寄信功能測試，請勿回覆!', 'plain', 'utf-8') # 信件內容
+msg['Subject'] = 'Do you see me?'    # 郵件標題
+msg['From'] = 'WorkingBuffer'        # 寄件者名
+msg['To'] = 'chungyou'               # 收件者名
+msg['Cc'] = 'abel@navibio.com.tw'    # 副本收件人信箱(需要再測試)
+message = msg.as_string()
 
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = smtp_user
-    msg['To'] = to_email
-
-    try:
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-            server.login(smtp_user, smtp_password)
-            server.sendmail(smtp_user, to_email, msg.as_string())
-        print(f"Email sent to {to_email}")
-    except Exception as e:
-        print(f"Failed to send email: {e}")
-    finally:
-        server.quit()
-
-# 設定條件變數，假設在這裡檢測到連線數大於2
-some_condition_detected = True
-
-if some_condition_detected:
-    send_email("PostgreSQL 警報", "查詢延遲過高，請立即檢查。", "dba_team@example.com")
-
-
-
-
-
-
-
-
-
-
-
-
-
+try:
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+      
+        server.login(smtp_user, smtp_password)
+        server.sendmail(smtp_user,'chungyou0118@gmail.com', message)
+    print('Email sent successfully')
+except Exception as e:
+    print('Email sent failed')
